6b9af2a0e0817698abadfbe0c4200638037280d7
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-09-13 18:31:29 -0400

    Initial project version

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>SEI Rocks!<h1>";
+mainEl.classList.add("flex-ctr");
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("--top-menu-bg");
+topMenuEl.classList.add("flex-around");
+
+
+
+
+
+// Iterate over the entire menuLinksarray and for each "link" object:
+
+menuLinks.forEach(function (link) {
+// Create an <a>element.
+const linkEl = document.createElement("a");
+
+// On the new element, add an hrefattribute with its value set to the hrefproperty of the "link" object.
+linkEl.setAttribute("href", link.href);
+
+// Set the new element's content to the value of the textproperty of the "link" object.
+linkEl.textContent = link.text;
+
+// Append the new element to the topMenuElelement.
+topMenuEl.appendChild(linkEl);
+});
+
+
+
