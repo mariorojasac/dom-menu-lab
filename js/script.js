92a37a4cf911d23cc3a8c008bdd7e448756f605c
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-09-13 20:40:45 -0400

    second commit

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var menuLinks = [
 ];
 const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = "var(--main-bg)";
-mainEl.innerHTML = "<h1>SEI Rocks!<h1>";
+mainEl.innerHTML = "<h1>SEI ROCKS!<h1>";
 mainEl.classList.add("flex-ctr");
 const topMenuEl = document.getElementById("top-menu");
 topMenuEl.style.height = "100%";
@@ -16,21 +16,14 @@ topMenuEl.classList.add("--top-menu-bg");
 topMenuEl.classList.add("flex-around");
 
 
-
-
-
 // Iterate over the entire menuLinksarray and for each "link" object:
-
 menuLinks.forEach(function (link) {
 // Create an <a>element.
 const linkEl = document.createElement("a");
-
 // On the new element, add an hrefattribute with its value set to the hrefproperty of the "link" object.
 linkEl.setAttribute("href", link.href);
-
 // Set the new element's content to the value of the textproperty of the "link" object.
 linkEl.textContent = link.text;
-
 // Append the new element to the topMenuElelement.
 topMenuEl.appendChild(linkEl);
 });
